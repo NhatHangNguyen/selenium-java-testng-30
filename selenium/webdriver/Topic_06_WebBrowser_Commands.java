526b2ab0d6fa9b97947ac072b89273aee2421bb8
Nhat Hang Nguyen <nhathangnguyen1806@gmail.com>
2025-06-25 23:13:54 +0700

    Web Browser Commands - Part 2

--- selenium/webdriver/Topic_06_WebBrowser_Commands.java
+++ selenium/webdriver/Topic_06_WebBrowser_Commands.java
@@ -1,14 +1,15 @@
 package webdriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.Logs;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
@@ -16,6 +17,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.lang.annotation.Target;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -25,6 +27,7 @@ import java.util.concurrent.TimeUnit;
 public class Topic_06_WebBrowser_Commands {
 
     WebDriver driver;
+    private Iterable<? extends Cookie> cookies;
 
     @BeforeClass
     public void initialBrowser() throws MalformedURLException {
@@ -43,7 +46,7 @@ public class Topic_06_WebBrowser_Commands {
     }
 
     @Test
-    public void TC_01_() {
+    public void TC_01_() throws MalformedURLException {
         // Mở ra 1 URL bất kì (lưu ý phải bắt đầu bằng http/ https)
         driver.get("https://demo.nopcommerce.com");
         driver.get("https://live.techpanda.org/index.php/catalog/seo_sitemap/category/");
@@ -73,37 +76,35 @@ public class Topic_06_WebBrowser_Commands {
         // Kiểm tra tuơng đối
         Assert.assertTrue(homePageSourceCode.contains("Conditions of Use"));
 
-        // Lấy ra ID của tab/ window đang active
-        driver.getWindowHandle();
-
-        // Lấy ra tất cả ID của tất cả các tab/ window đang có
-        driver.getWindowHandles();
-
         // Đi tìm 1 element
         driver.findElement(By.xpath(""));
 
         // Đi tìm n elements
         driver.findElements(By.xpath(""));
 
-        // Selenium ver 3
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.DAYS);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.HOURS);
+        WebDriver.Options options = driver.manage();
 
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.MINUTES);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        // Selenium ver 3
+        options.timeouts().implicitlyWait(15, TimeUnit.DAYS);
+        options.timeouts().implicitlyWait(15, TimeUnit.HOURS);
+
+        options.timeouts().implicitlyWait(15, TimeUnit.MINUTES);
+        options.timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.MILLISECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.MICROSECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.NANOSECONDS);
 
+        WebDriver.Timeouts timeouts = driver.manage().timeouts();
+
         // Selenium ver 4
         // Dùng để chờ cho việc tìm element (findElement/ findElements)
-        driver.manage().timeouts().implicitlyWait(Duration.ofDays(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofHours(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofNanos(15));
+        timeouts.implicitlyWait(Duration.ofDays(15));
+        timeouts.implicitlyWait(Duration.ofHours(15));
+        timeouts.implicitlyWait(Duration.ofMinutes(15));
+        timeouts.implicitlyWait(Duration.ofSeconds(15));
+        timeouts.implicitlyWait(Duration.ofMillis(15));
+        timeouts.implicitlyWait(Duration.ofNanos(15));
 
         // Dùng để chờ cho việc page đươc load xong
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
@@ -112,14 +113,16 @@ public class Topic_06_WebBrowser_Commands {
         // JavascriptExecutor - js
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(15));
 
+        WebDriver.Window window = driver.manage().window();
+
         // Thu nhỏ về taskbar để chạy
-        driver.manage().window().minimize();
+        window.minimize();
 
         // Phóng to lên  (vẫn còn có taskbar)
-        driver.manage().window().maximize();
+        window.maximize();
 
         // Tràn màn hình (không có taskbar)
-        driver.manage().window().fullscreen();
+        window.fullscreen();
 
         // Responsive
         driver.manage().window().setSize(new Dimension(1920,1080));
@@ -130,6 +133,74 @@ public class Topic_06_WebBrowser_Commands {
 
         // Test GUI: Graphic User Interface
         // Front/ Color/ Size/ Position/ Location/...
+
+        // Lấy hết tất cả Cookie
+        driver.manage().getCookies();
+
+        driver.manage().getCookieNamed("");
+
+        // Xoá hết Cookie
+        driver.manage().deleteAllCookies();
+
+        for (Cookie cookie : cookies) {
+            // Xoá cookie theo thứ tự
+            driver.manage().deleteCookie(cookie);
+        }
+
+        // Xoá cookie theo tên
+        driver.manage().deleteCookieNamed("");
+
+        // Đến 1 test class khác 02/03/04/... (Không cần login - set cookie đã có vào đây rồi refesh lại)
+        for (Cookie cookie : cookies) {
+            // Add cookie theo thứ tự
+            driver.manage().addCookie(cookie);
+        }
+        driver.navigate().refresh(); // Login thành công
+
+        Logs log = driver.manage().logs();
+        LogEntries logEntries = log.get("BROWSER");
+
+        for(LogEntry logEn: logEntries){
+            System.out.println(logEn);
+        }
+
+        log.getAvailableLogTypes();
+
+        WebDriver.Navigation navigation = driver.navigate();
+
+        // Refresh/ F5
+        navigation.refresh();
+
+        // Quay lại trang trước đó
+        navigation.back();
+
+        // Chuyển tiếp tới trang trước đó
+        navigation.forward();
+
+        // Mở URL bất kì
+        navigation.to("https://demo.nopcommerce.com");
+
+        navigation.to(new URL("https://demo.nopcommerce.com"));
+
+        // Alert/ Iframe/ Window (tab)
+        WebDriver.TargetLocator targetLocator = driver.switchTo();
+
+        // Alert
+        targetLocator.alert().accept();
+        targetLocator.alert().dismiss();
+
+        // Frame/ Iframe
+        targetLocator.frame("");
+        targetLocator.defaultContent();
+
+        // Window
+        targetLocator.window("");
+
+        // Lấy ra ID của tab/ window đang active
+        driver.getWindowHandle();
+
+        // Lấy ra tất cả ID của tất cả các tab/ window đang có
+        driver.getWindowHandles();
     }
 
 
---
+++ selenium/webdriver/Topic_07_WebElement_Commands.java
@@ -0,0 +1,58 @@
+package webdriver;
+
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Topic_07_WebElement_Commands {
+
+    // Chứa các hàm để tương tác với browser
+    WebDriver driver;
+
+    WebElement element;
+
+    @Test
+    public void TC_01_WebElement() {
+        driver.findElement(By.xpath("")).click();
+
+        element = driver.findElement(By.xpath(""));
+
+        // Click vào các element dạng: button/ checkbox/ radio/ link/ image/ icon....
+        element.click();
+
+        // Nhập liệu các element dang: textbox/ textarea/ dropdow (edit)
+        // Xoá dữ liệu trước khi sendkey
+        element.clear();
+        element.sendKeys("test@gmail.com");
+        element.sendKeys(Keys.COMMAND);
+
+        driver.findElement(By.id("div.login-page"))
+                .findElement(By.cssSelector("div.customer-blocks"))
+                .findElement(By.id("Email"));
+
+        driver.findElement(By.cssSelector("div.login-page div.customer-blocks input#Email"));
+
+        // Tác dụng vs form (SignUp/ Login/ Search/...)
+        // thẻ form
+        driver.findElement(By.id("Email")).sendKeys("");
+        driver.findElement(By.id("Password")).sendKeys("");
+        driver.findElement(By.id("Password")).submit();
+
+        // Áp dụng cho tất cả các loai element
+        // Kiểm tra 1 element có hiện thị hay không
+        // Size > 0: Width/ Height > 0
+        // Nhìn thấy/ thao tác được
+        element.isDisplayed();
+
+        Assert.assertTrue(element.isDisplayed());
+        Assert.assertFalse(element.isDisplayed());
+
+        // Áp dụng cho duy nhất 3 loại: checkbox/ radio/ dropdown (default)
+        // Kiểm tra 1 element đã được chọn rồi hay chưa chọn
+        element.isSelected();
+
+        // Áp dụng cho tất cả các loại
+        // Kiểm tra 1 element có bị disable hay không (read only)
+        element.isEnabled();
+    }
+}
