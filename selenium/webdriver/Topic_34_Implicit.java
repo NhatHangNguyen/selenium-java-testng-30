831ad29f267ac2bba95116bab3130bdaa54d5069
Nhat Hang Nguyen <nhathangnguyen1806@gmail.com>
2025-09-10 21:43:07 +0700

    Wait Part III - Implicit and Static Wait

---
+++ selenium/webdriver/Topic_34_Implicit.java
@@ -0,0 +1,65 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_34_Implicit {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser(){
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void TC_01_Dont_Set(){
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_02_Les_Than(){
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_03_Equal(){
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_04_Greater_Than(){
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @AfterClass
+    public void cleanBrowser(){
+        driver.quit();
+    }
+}
---
+++ selenium/webdriver/Topic_35_Static.java
@@ -0,0 +1,60 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_35_Static {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser(){
+
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void TC_01_Les_Than() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Thread.sleep(3000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_02_Equal() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Thread.sleep(5000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_03_Greater_Than() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Thread.sleep(10000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @AfterClass
+    public void cleanBrowser(){
+        driver.quit();
+    }
+}
