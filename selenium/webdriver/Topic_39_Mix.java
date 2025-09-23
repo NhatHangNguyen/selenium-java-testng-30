98bc0d734d90b0a6a9ecf401d17f8686daabffa0
Nhat Hang Nguyen <nhathangnguyen1806@gmail.com>
2025-09-23 21:04:47 +0700

    Wait Part III - Mix Wait

---
+++ selenium/webdriver/Topic_39_Mix.java
@@ -0,0 +1,114 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Date;
+
+public class Topic_39_Mix {
+    WebDriver driver;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void initialBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Element_Found(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        // Wait vs Explicit
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email")));
+
+        // Wait vs Implicit
+        driver.findElement(By.cssSelector("input#email"));
+    }
+
+    @Test
+    public void TC_02_Element_Not_Found_Only_Implicit(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        // Wait vs Implicit
+        driver.findElement(By.cssSelector("input#automationfc"));
+    }
+
+    @Test
+    public void TC_03_Element_Not_Found_Only_Emplicit_By(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        By emailTextboxBy = By.cssSelector("inpunt#automationfc");
+
+        // Implicit = 0
+        // Explicit = 3
+        // Wait vs Explicit
+        System.out.println("Start = " + getDateTimeNow());
+        try {
+            explicitWait.until(ExpectedConditions.visibilityOfElementLocated(emailTextboxBy));
+        } catch (Exception e) {
+            System.out.println("End =  " + getDateTimeNow());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void TC_03_Element_Not_Found_Only_Emplicit_WebElement(){
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        // Implicit = 0
+        // Explicit = 3
+        // Wait vs Explicit
+        System.out.println("Start =  " + getDateTimeNow());
+        try {
+            explicitWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("input#automationfc"))));
+        } catch (Exception e) {
+            System.out.println("End =  " + getDateTimeNow());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void TC_04_Element_Not_Found_Only_Emplicit(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(8));
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        System.out.println("Start = " + getDateTimeNow());
+
+        // Wait vs Explicit
+        try {
+            explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input@automationfc")));
+        } catch (Exception e) {
+            System.out.println("End = " + getDateTimeNow());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getDateTimeNow(){
+        Date date = new Date();
+        return date.toString();
+    }
+
+    @AfterClass
+    public void cleanBrowser(){
+        driver.quit();
+    }
+}
